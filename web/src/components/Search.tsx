6b5360fece24acea5ef96515169e30ab9416292a
Thaninpat <thaninpat.o@gmail.com>
2021-02-04 11:01:43 +0700

    revert commit

--- web/src/components/Search.tsx
+++ web/src/components/Search.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Center, Box, Input } from '@chakra-ui/react'
 import React from 'react'
 
@@ -26,3 +27,33 @@ export const Search: React.FC<SearchProps> = ({
     </Center>
   )
 }
+=======
+import { Center, Box, Input } from '@chakra-ui/react'
+import React from 'react'
+
+interface SearchProps {
+  value: string
+  onValueChange: any
+}
+
+export const Search: React.FC<SearchProps> = ({
+  value,
+  onValueChange,
+}: SearchProps) => {
+  return (
+    <Center marginTop='30px'>
+      <Box padding='10px'>
+        <Input
+          variant='flushed'
+          placeholder='Search'
+          type='text'
+          value={value}
+          onChange={(event) => {
+            onValueChange(event.target.value)
+          }}
+        />
+      </Box>
+    </Center>
+  )
+}
+>>>>>>> de6edc37dde3cf299b819a2c79b87d987b6dd5a3
--- web/src/pages/user.tsx
+++ web/src/pages/user.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Container, Flex, Heading, Input, Stack } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { useUserQuery } from '../generated/graphql'
@@ -36,3 +37,43 @@ const user: React.FC<userProps> = ({}) => {
 }
 
 export default user
+=======
+import { Container, Flex, Heading, Input, Stack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useUserQuery } from '../generated/graphql'
+
+interface userProps {}
+
+const user: React.FC<userProps> = ({}) => {
+  const [addUser, setAddUser] = useState('')
+  const [addRole, setAddRole] = useState('')
+
+  return (
+    <Flex align={'center'} justify={'center'} p={20} flexDirection={'column'}>
+      <Heading>User page</Heading>
+      <Container margin={10}>
+        <Stack spacing={4}>
+          <Input
+            type='text'
+            placeholder='Input user'
+            value={addUser}
+            onChange={(event) => {
+              setAddUser(event.target.value)
+            }}
+          />
+          <Input
+            type='text'
+            placeholder='Input role'
+            value={addRole}
+            onChange={(event) => {
+              setAddRole(event.target.value)
+            }}
+          />
+        </Stack>
+      </Container>
+    </Flex>
+  )
+}
+
+export default user
+>>>>>>> de6edc37dde3cf299b819a2c79b87d987b6dd5a3
